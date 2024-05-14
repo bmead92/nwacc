7ed531484d0b3ae1fca85bcedd028debce9d6097
Bryce Meadors <bryce.meadors@gmail.com>
2024-05-14 12:16:54 -0700

    added media queries

--- src/pages/Autocross.js
+++ src/pages/Autocross.js
@@ -2,19 +2,19 @@ import React from "react";
 import Header from "../components/LandingPage/Header/Header";
 import Strings from "../data/strings/Strings";
 import NavigationTabs from "../components/LandingPage/NavigationTabs/NavigationTabs";
-import Documents from "../components/Autocross/Documents/Documents";
+import Autocross from "../components/Autocross/Autocross";
 import "../components/LandingPage/NavigationTabs/NavigationTabs.scss";
 import "../App.scss";
 
-const Autocross = () => {
+const AutocrossPage = () => {
   const header = Strings.autocross.pageStrings.header;
   return (
     <div className="backgroundImage autocross">
       <NavigationTabs />
       <Header title={header} />
-      <Documents />
+      <Autocross />
     </div>
   );
 };
 
-export default Autocross;
+export default AutocrossPage;

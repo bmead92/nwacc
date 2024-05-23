6bbdcb4128231555e8f7f7c62ab076e129222947
Bryce Meadors <bryce.meadors@gmail.com>
2024-05-23 09:16:09 -0700

    fixed ac page

--- src/components/autocross/Autocross.js
+++ src/components/autocross/Autocross.js
@@ -4,29 +4,21 @@ import PdfRenderer from "../shared/pdf/PdfRenderer";
 
 const Autocross = () => {
   return (
-    <div className="mainContainer">
-      <div className="documentsContainer">
-        <h2 className="documentsHeader">
-          {Strings.autocross.pageStrings.documentsHeader}
-        </h2>
-        {Strings.autocross.documents.map((doc) => {
-          return (
-            <>
-              <h2 className="autocrossDocs">{doc.name}</h2>
-              <PdfRenderer url={doc.url} file={doc.url} />
-              <a className="autocrossLinks" href={doc.url} download>
-                {Strings.pdfDownload}
-              </a>
-            </>
-          );
-        })}
-      </div>
-      {/* <div className="eventsContainer">
-        <h2 className="eventsHeader">
-          {Strings.autocross.pageStrings.eventsHeader}
-        </h2>
-        <p>{Strings.autocross.pageStrings.eventsText}</p>
-      </div> */}
+    <div className="autocrossContainer">
+      <h2 className="documentsHeader">
+        {Strings.autocross.pageStrings.documentsHeader}
+      </h2>
+      {Strings.autocross.documents.map((doc) => {
+        return (
+          <>
+            <h2 className="autocrossDocs">{doc.name}</h2>
+            <PdfRenderer url={doc.url} file={doc.url} />
+            <a className="autocrossLinks" href={doc.url} download>
+              {Strings.pdfDownload}
+            </a>
+          </>
+        );
+      })}
     </div>
   );
 };

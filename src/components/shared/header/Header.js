f4372ab8818ee7d76219f5f29059f94a0fca9769
Bryce Meadors <bryce.meadors@gmail.com>
2024-05-23 14:14:31 -0700

    refactor menu, added logo

--- src/components/shared/header/Header.js
+++ src/components/shared/header/Header.js
@@ -1,10 +1,12 @@
 import "./Header.scss";
 import React from "react";
+import logo from "../../../assets/images/logo.jpg";
 
 const Header = ({ title }) => {
   return (
     <div className="headerContainer">
       <h1 className="headerText">{title}</h1>
+      <img className="logo" src={logo} alt="NWACC logo" />
     </div>
   );
 };
